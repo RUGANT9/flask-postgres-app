ef5fcd48ef9800b6391986cdb66d708db06d5d58
devodutt98 <devodutt98@gmail.com>
2025-06-11 16:39:38 -0700

    trigger test 2

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ def index():
             host=os.environ["POSTGRES_HOST"],
             port=os.environ.get("POSTGRES_PORT", 5432)
         )
-        return "✅ Connected to PostgreSQL and added the Jenkins CI/CD! //checking if pipeline trigger for change - 1"
+        return "✅ Connected to PostgreSQL and added the Jenkins CI/CD! //checking if pipeline trigger for change - 2"
     except Exception as e:
         return f"❌ Failed to connect: {e}"
 
